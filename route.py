10ba6dfd9dbb335f9b1273584819e206c52e12ed
Taylor Greiner <taylor.greiner@gmail.com>
2023-11-13 09:09:28 -0600

    Update route.py

--- route.py
+++ route.py
@@ -2,7 +2,7 @@ from azure.maps.route import MapsRouteClient
 from azure.core.credentials import AzureKeyCredential
 
 
-def get_drive_time(start_address, end_address, subscription_key):
+def get_drive_time(subscription_key):
     # Create a client
     credential = AzureKeyCredential(subscription_key)
     client = MapsRouteClient(credential)
@@ -11,12 +11,11 @@ def get_drive_time(start_address, end_address, subscription_key):
     result = client.get_route_directions(route_points=[("41.708250", "-93.793730"), ("41.586834", "-93.624962")]);
     
     print("Get Route Directions with list of coordinates:")
-#    print(result.routes[0].summary)
+    print(result.routes[0].summary)
     print(result.routes[0].sections[0])
 
 
 # Example usage
 subscription_key = "SUBSCRIPTION_KEY"
-duration = get_drive_time(start_address, end_address, subscription_key)
-print(f"Drive time in seconds: {duration}")
+duration = get_drive_time(subscription_key)
 
